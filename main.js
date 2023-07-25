0b8f88919bb8c2e8b807b419e2c564c65c027635
vinicius natan <vinicius.natan.dev@gmail.com>
2023-07-25 14:02:32 -0300

    validado

--- main.js
+++ main.js
@@ -1,15 +1,20 @@
+// pegar os elementos html para o javascript
 const form = document.getElementById('form-numeros');
 const numeroAinput = document.getElementById('numero-a');
 const numeroBinput = document.getElementById('numero-b');
 const certoMessage = document.querySelector('.certo');
 const erradoMessage = document.querySelector('.errado');
 
+// adicionar ouvidor de eventos no evento submit e
 form.addEventListener('submit', function (e) {
+    // funçao de tirar o comportamento padrao do submit
     e.preventDefault();
 
+    // pegar o valor dos 2 inputs e armazenar em uma const
     const numeroA = parseFloat(numeroAinput.value);
     const numeroB = parseFloat(numeroBinput.value);
 
+    // validaçao e imprimir mensagens
     if (numeroB > numeroA) {
         certoMessage.style.display = 'block';
         erradoMessage.style.display = 'none';
@@ -23,4 +28,4 @@ form.addEventListener('submit', function (e) {
     }
 
 
-});
+})
